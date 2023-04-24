a31cb6f2c3ed429cc70b0abf23ad70a30027bd66
CristianMira <krhistian1902@gmail.com>
2023-04-23 23:41:04 -0500

    pages

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid recusandae error similique? Quisquam optio maxime, molestias labore quidem voluptatem corrupti ratione neque nulla, odit recusandae sit ea reprehenderit nesciunt fugit deleniti a explicabo. Magni nam beatae provident modi tempore eius, cum voluptates odio placeat voluptatum voluptatibus expedita natus, architecto minima commodi tenetur animi obcaecati nesciunt atque odit voluptatem? Impedit voluptatem ut incidunt asperiores ducimus consequatur odit rerum assumenda fugiat dicta eum ea, amet placeat libero eius! Cumque tempora minus itaque facilis ipsam repudiandae aperiam corrupti dolorum fugiat quas fuga ipsum sed quia, consequuntur aliquid iure molestias nihil ad libero non.</p>
+    </div>
+   
+  )
+}
+
+export default About
---
+++ app/services/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>
+      <h2>Services</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatum, tempora fuga officia, aperiam qui assumenda repellat iure excepturi iusto quia aspernatur sed ipsam id ab, laudantium quae eveniet. Eligendi itaque libero illo reprehenderit fugiat similique harum id fugit optio deserunt placeat veritatis autem neque dolorum, voluptas rerum magnam a vitae totam dicta nostrum porro atque. Qui sed saepe animi, amet adipisci tenetur! Suscipit, eos laborum? Enim rem assumenda asperiores magnam expedita facere soluta possimus sunt veritatis, qui inventore harum facilis quaerat cum, perspiciatis in aspernatur quam. Ex dolores quaerat quas assumenda, reiciendis qui placeat corporis tenetur necessitatibus beatae officiis.</p>
+    </div>
+  )
+}
+
+export default Services
