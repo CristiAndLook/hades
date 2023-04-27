7e2375352a94a53d2fc118f482b896fa697e764c
CristianMira <krhistian1902@gmail.com>
2023-04-26 20:29:25 -0500

    Fetch for Id

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+async function getUser(id) {
+  // ...  fetch user data
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  return data.data
 
-function UsersPage() {
+}
+
+async function UsersPage({params}) {
+  const user = await getUser(params.id)
   return (
-    <div>Users Detail</div>
+    <div>
+      <h1>User Page</h1>
+      <img src={user.avatar} alt={user.first_name} />
+      <h3>{user.first_name} {user.last_name}</h3>
+      <p>{user.email}</p>
+    
+    </div>
   )
 }
 
