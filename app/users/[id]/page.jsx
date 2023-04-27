90a9be5f27dc3254f607cd441b863264acd63430
CristianMira <krhistian1902@gmail.com>
2023-04-26 21:15:29 -0500

    Styles

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -9,11 +9,19 @@ async function getUser(id) {
 async function UsersPage({params}) {
   const user = await getUser(params.id)
   return (
-    <div>
-      <h1>User Page</h1>
-      <img src={user.avatar} alt={user.first_name} />
-      <h3>{user.first_name} {user.last_name}</h3>
-      <p>{user.email}</p>
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <div className="card">
+          <div className="card-header text-center">
+            <img src={user.avatar} alt={user.first_name} />
+          </div>
+          <div className="card-body text-center">
+            <h3>{user.first_name} {user.last_name}</h3>
+            <p>{user.email}</p>
+          </div>
+        </div>
+      </div>
+     
     
     </div>
   )
--- components/Users.jsx
+++ components/Users.jsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/navigation"
 function Users({ users }) {
   const router = useRouter()
   return (
-    <ul>
+    <ul className="list-group">
         {users.map(user => (
           <li key={user.id}
+          className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
             onClick={() => router.push(`/users/${user.id}`)}
           >
             <p>{user.first_name} {user.last_name}</p>
-            <img src={user.avatar} alt={user.first_name} />
+            <img src={user.avatar} alt={user.first_name} style={{borderRadius: "50%"}}/>
           </li>
         ))}
       </ul>  
